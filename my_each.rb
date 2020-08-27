89368fac0c4cc4e3a3699605c772eef155482103
Dario Carlino <baires82@gmail.com>
2020-08-27 22:36:43 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,6 +1,10 @@
 def my_each(array)
   i = 0
+<<<<<<< HEAD
   while i != array.length
+=======
+  while i >= 0
+>>>>>>> 9d03f3a9ebe031865359ac99a1e5c001a91d881a
       yield(array[i])
       i = i + 1
   end
